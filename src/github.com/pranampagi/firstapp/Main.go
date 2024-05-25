5d7edcc2f0e95cc986941a01253314d6ba82d3ee
Pranam Pagi <pranampagi@gmail.com>
2024-05-25 17:21:41 +0530

    Primitives

--- src/github.com/pranampagi/firstapp/Main.go
+++ src/github.com/pranampagi/firstapp/Main.go
@@ -1,15 +1,66 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
 func main() {
-	var i int = 42
-	fmt.Printf("%v, %T\n", i, i)
 
-	var j string
-	j = strconv.Itoa(i)
-	fmt.Printf("%v, %T\n", j, j)
+	// n := 1 == 1
+	// m := 1 == 2
+	// fmt.Printf("%v, %T\n", n, n)
+	// fmt.Printf("%v, %T\n", m, m)
+
+	// var n bool
+	// fmt.Printf("%v, %T\n", n, n)
+
+	// a := 10 // 1010
+	// b := 3  // 0011
+	// fmt.Println(a + b)
+	// fmt.Println(a - b)
+	// fmt.Println(a * b)
+	// fmt.Println(a / b)
+	// fmt.Println(a % b)
+	// fmt.Println(a & b)  // 0010
+	// fmt.Println(a | b)  // 1011
+	// fmt.Println(a ^ b)  // 1001
+	// fmt.Println(a &^ b) // 0100
+
+	// a := 8 						// 2^3
+	// fmt.Println(a << 3) // 2^3 * 2^3 = 2^6
+	// fmt.Println(a >> 3) // 2^3 / 2^3 = 2^0
+
+	// var n float64 = 3.14
+	// n = 13.7e72
+	// n = 2.1e14
+	// fmt.Printf("%v, %T\n", n, n)
+
+	// a := 10.2
+	// b := 3.7
+	// fmt.Println(a + b)
+	// fmt.Println(a - b)
+	// fmt.Println(a * b)
+	// fmt.Println(a / b)
+
+	// var n complex64 = 1 + 2i
+	// fmt.Printf("%v, %T\n", n, n)
+	// fmt.Printf("%v, %T\n", real(n), real(n))
+	// fmt.Printf("%v, %T\n", imag(n), imag(n))
+
+	// a := 1 + 2i
+	// b := 2 + 5.2i
+	// fmt.Println(a + b)
+	// fmt.Println(a - b)
+	// fmt.Println(a * b)
+	// fmt.Println(a / b)
+
+	// var n complex128 = complex(5, 12)
+	// fmt.Printf("%v, %T\n", n, n)
+
+	// s := "this is a string"
+	// b := []byte(s)
+	// fmt.Printf("%v, %T\n", s[2], s[2])
+	// fmt.Printf("%v, %T\n", b, b)
+
+	var r rune = 'a'
+	fmt.Printf("%v, %T\n", r, r)
+
 }
